03933462fee77f06b7c7310ae5662d3dd8d11af3
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-07-11 00:02:28 -0300

    update sdk-core

--- src/multicallExtended.ts
+++ src/multicallExtended.ts
@@ -1,5 +1,5 @@
 import { Interface } from '@ethersproject/abi'
-import { BigintIsh } from '@uniswap/sdk-core'
+import { BigintIsh } from '@thinkincoin-libs/sdk-core'
 import { abi } from '@uniswap/swap-router-contracts/artifacts/contracts/interfaces/IMulticallExtended.sol/IMulticallExtended.json'
 import { Multicall, toHex } from '@uniswap/v3-sdk'
 
